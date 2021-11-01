d6ce141c25b2346f6d5c196f9603f2d6365a8567
陈新 <chenx@mti-sh.com>
2021-11-01 21:09:06 +0800

    gcm业务修改

--- app/src/main/java/com/btc/application/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/btc/application/ui/dashboard/DashboardFragment.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -24,12 +22,13 @@ public class DashboardFragment extends Fragment {
                 new ViewModelProvider(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
 //        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        /*dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
 //                textView.setText(s);
             }
-        });
+        });*/
+
         return root;
     }
 }
