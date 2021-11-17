01ff7780a1c2bf475556df443579730f3fd0a0fe
陈新 <chenx@mti-sh.com>
2021-11-17 21:53:51 +0800

    gcm业务修改

--- app/src/main/java/com/btc/application/ui/dashboard/SellFragment.java
+++ app/src/main/java/com/btc/application/ui/dashboard/SellFragment.java
@@ -3,10 +3,12 @@ package com.btc.application.ui.dashboard;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,18 +20,44 @@ import androidx.lifecycle.ViewModelProvider;
 import com.btc.application.myapplication.R;
 import com.btc.application.ui.extend.ExtendsViewModel;
 import com.btc.application.ui.login.LoginActivity;
+import com.btc.application.util.HttpUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class SellFragment extends Fragment {
 
-//    private ExtendsViewModel extendsViewModel;
+    private TextView floatRatio;
+    private TextView currentRrice;
+    private TextView currentAveragePrice;
+    private TextView currentHighPrice;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-//        extendsViewModel = new ViewModelProvider(this).get(ExtendsViewModel.class);
-//        View root = inflater.inflate(R.layout.fragment_extends, container, false);
         View root = inflater.inflate(R.layout.fragment_sell, container, false);
-        /*final Button buttonFirst = root.findViewById(R.id.button_first);
-        buttonFirst.setTextColor(Color.rgb(0, 0, 0));*/
+        floatRatio = root.findViewById(R.id.float_ratio);
+        currentRrice = root.findViewById(R.id.current_price);
+        currentAveragePrice = root.findViewById(R.id.current_average_price);
+        currentHighPrice = root.findViewById(R.id.current_high_price);
+
+        String method = "user/getConstant/";
+        String result = HttpUtils.getJsonByInternet(method);
+        Log.d("debugTest",result);
+
+        try {
+            JSONObject jsonObject1 = new JSONObject(result);
+            String code = jsonObject1.getString("code");
+            if ("000000".equals(code))
+            {
+                JSONObject data = jsonObject1.getJSONObject("data");
+                floatRatio.setText(data.getString("floatRatio") + "%");
+                currentRrice.setText(data.getString("currentRrice") );
+                currentAveragePrice.setText(data.getString("currentAveragePrice") );
+                currentHighPrice.setText(data.getString("currentHighPrice") );
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
         return root;
     }
