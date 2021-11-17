1e214edbd01ba6bc41fbf2df02d411079eb4ba60
陈新 <chenx@mti-sh.com>
2021-11-17 23:21:24 +0800

    gcm业务修改

--- app/src/main/java/com/btc/application/ui/dashboard/SellFragment.java
+++ app/src/main/java/com/btc/application/ui/dashboard/SellFragment.java
@@ -8,6 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -25,12 +27,19 @@ import com.btc.application.util.HttpUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class SellFragment extends Fragment {
 
     private TextView floatRatio;
     private TextView currentRrice;
     private TextView currentAveragePrice;
     private TextView currentHighPrice;
+
+    List<String> list;
+    List<String> list1;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -58,6 +67,34 @@ public class SellFragment extends Fragment {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+
+
+        ListView listView = (ListView) root.findViewById(R.id.listView);
+
+        //获取到集合数据
+        //名字列表，之后可以动态加入数据即可，这里只是数据例子
+        list = new ArrayList<>();
+        list.add("小明");
+        list.add("李华");
+        list.add("张三");
+
+        list1 = new ArrayList<>();
+        list1.add("男");
+        list1.add("男");
+        list1.add("女");
+
+        List<HashMap<String, Object>> data = new ArrayList<HashMap<String,Object>>();
+        for(int i = 0; i < list .size(); i++){
+            HashMap<String, Object> item = new HashMap<String, Object>();
+            item.put("name", list.get(i));
+            item.put("sex", list1.get(i));
+            data.add(item);
+        }
+        //创建SimpleAdapter适配器将数据绑定到item显示控件上
+        SimpleAdapter adapter = new SimpleAdapter(root.getContext(), data, R.layout.item,
+                new String[]{"name", "sex"}, new int[]{R.id.name, R.id.sex});
+        //实现列表的显示
+        listView.setAdapter(adapter);
 
         return root;
     }
