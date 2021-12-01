12bb6508ed8c5c9f062d56ee3c5a0a7ff9727577
陈新 <chenx@mti-sh.com>
2021-12-01 21:39:28 +0800

    gcm业务修改

--- app/src/main/java/com/btc/application/myapplication/PublishOrderActivity.java
+++ app/src/main/java/com/btc/application/myapplication/PublishOrderActivity.java
@@ -3,13 +3,18 @@ package com.btc.application.myapplication;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.btc.application.MainActivity;
 import com.btc.application.ui.dashboard.DashboardFragment;
+import com.btc.application.util.HttpUtils;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -30,16 +35,23 @@ public class PublishOrderActivity extends AppCompatActivity {
 // 接收参数，参数类型可以是八大基本类型、String、八大基本类型的数组
         Object flag = bundle.getString(DashboardFragment.FLAG);
 
+        String method = null;
+        String successTip = null;
+
         if (null != flag)
         {
             String flagStr = flag.toString();
             if ("sell".equals(flagStr))
             {
                 toolbar.setTitle("发布出售信息");
+                successTip = "发布出售信息成功!";
+                method = "user/publishSellInfo";
             }
             else if ("buy".equals(flagStr))
             {
                 toolbar.setTitle("发布购买信息");
+                successTip = "发布购买信息成功!";
+                method = "user/publishBuyInfo";
             }
         }
 
@@ -47,8 +59,11 @@ public class PublishOrderActivity extends AppCompatActivity {
 
         final TextView inputNum = findViewById(R.id.input_num);
         final TextView minPrice = findViewById(R.id.min_price);
+        final TextView unitPrice = findViewById(R.id.unit_price);
         final Button cbtPublish = findViewById(R.id.cbt_publish);
 
+        String finalMethod = method;
+        String finalSuccessTip = successTip;
         cbtPublish.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -59,14 +74,14 @@ public class PublishOrderActivity extends AppCompatActivity {
                 JSONObject jsonObject = new JSONObject();
                 try {
                     jsonObject.put("id", id);
-                    jsonObject.put("secondPwd", inputNum.getText().toString());
-                    jsonObject.put("confirmSecondPwd", minPrice.getText().toString());
+                    jsonObject.put("num", inputNum.getText().toString());
+                    jsonObject.put("min", minPrice.getText().toString());
+                    jsonObject.put("price", unitPrice.getText().toString());
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
 
-                /*String method = "user/set2ndPwd";
-                String result = HttpUtils.sendJsonPost(jsonObject.toString(), method , "POST");
+                String result = HttpUtils.sendJsonPost(jsonObject.toString(), finalMethod, "POST");
                 Log.v(TAG , result);
                 try {
                     JSONObject jsonObject1 = new JSONObject(result);
@@ -74,8 +89,8 @@ public class PublishOrderActivity extends AppCompatActivity {
                     if ("000000".equals(code))
                     {
                         Log.v(TAG , code);
-                        Toast.makeText(getApplicationContext(), "修改成功！", Toast.LENGTH_LONG).show();
-                        startActivity(new Intent(PublishOrderActivity.this, MainActivity.class));
+                        Toast.makeText(getApplicationContext(), finalSuccessTip, Toast.LENGTH_LONG).show();
+                        startActivity(new Intent(v.getContext(), MainActivity.class));
                     }
                     else
                     {
@@ -83,7 +98,7 @@ public class PublishOrderActivity extends AppCompatActivity {
                     }
                 } catch (JSONException e) {
                     e.printStackTrace();
-                }*/
+                }
             }
         });
     }
