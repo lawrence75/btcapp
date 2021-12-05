b279149fcdabd2b57e690475d3e4951620195a8b
陈新 <chenx@mti-sh.com>
2021-12-05 21:22:33 +0800

    gcm业务修改

--- app/src/main/java/com/btc/application/myapplication/BuyDetailActivity.java
+++ app/src/main/java/com/btc/application/myapplication/BuyDetailActivity.java
@@ -1,8 +1,13 @@
 package com.btc.application.myapplication;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +16,14 @@ import androidx.appcompat.widget.Toolbar;
 import com.btc.application.ui.dashboard.BuyFragment;
 import com.btc.application.ui.dashboard.SellFragment;
 import com.btc.application.util.Constant;
+import com.btc.application.util.FileUtils;
+import com.btc.application.util.HttpUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class BuyDetailActivity extends AppCompatActivity {
 
@@ -36,6 +49,7 @@ public class BuyDetailActivity extends AppCompatActivity {
         final TextView inputNum = findViewById(R.id.buy_num);
         final TextView inputMin = findViewById(R.id.buy_min_price);
         final TextView inputPrice = findViewById(R.id.buy_unit_price);
+        final ImageView receiveCodeImage = findViewById(R.id.receive_code_image);
         final Button buyBtn = findViewById(R.id.cbt_buy);
 
         if (null != num)
@@ -53,5 +67,63 @@ public class BuyDetailActivity extends AppCompatActivity {
             String priceStr = price.toString();
             inputPrice.setText(Constant.LABEL_PRICE + priceStr + Constant.BLANK + Constant.CNY);
         }
+
+        // 获取SharedPreference
+        SharedPreferences preference = getWindow().getContext().getSharedPreferences("userinfo", MODE_PRIVATE);
+        // 获取存在SharedPreference中的用户名
+        id = preference.getInt("id", 0);
+
+        String method = "user/getUser/";
+        String result = HttpUtils.getJsonByInternet(method+id);
+        Log.d("debugTest",result);
+
+        JSONObject data = new JSONObject();
+        Integer defaultPay = 0;
+        String imageUrl = "";
+
+        try {
+            JSONObject jsonObject1 = new JSONObject(result);
+            String code = jsonObject1.getString("code");
+            if ("000000".equals(code))
+            {
+                data = jsonObject1.getJSONObject("data");
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        String wechatAddress = null;
+        String alipayAddress = null;
+        String bankCard = null;
+        try {
+            wechatAddress = data.getString("wechatAddress");
+            alipayAddress = data.getString("alipayAddress");
+            bankCard = data.getString("bankCard");
+            defaultPay = data.getInt("defaultPay");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        switch(defaultPay)
+        {
+            case 1:
+            {
+                imageUrl = wechatAddress;
+                break;
+            }
+            case 2:
+            {
+                imageUrl = alipayAddress;
+                break;
+            }
+            case 3:
+            {
+                imageUrl = bankCard;
+                break;
+            }
+        }
+
+        Bitmap bitmap = FileUtils.url2bitmap(Constant.APP_URL + Constant.FILE_PREFIX + imageUrl);
+        receiveCodeImage.setImageBitmap(bitmap);
     }
 }
